ff8bb2c0cdf2b4f4abcfc36df788e813c53ed5f1
Abhishek Agrahari <abhishek737333@gmail.com>
2024-06-25 16:02:55 +0530

    added Signin fucntionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
   if (
@@ -23,3 +24,25 @@ export const signup = async (req, res,next) => {
     next(error); 
   }
 };
+
+export const signin = async (req,res,next) =>{
+  const {email,password} = req.body;
+  if(!email || !password || email === '' || password === ''){
+   return next(errorHandler(400,'All fields are required'));
+  }
+  try {
+    const validUser = await User.findOne({email});
+    if(!validUser){
+     return next(errorHandler(404,'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword){
+     return next(errorHandler(400,'Invalid Password'));
+    }
+    const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+    res.status(200).cookie('access_token',token,{
+      httpOnly:true}).json(validUser);
+  } catch (error) {
+    next(error);
+  }
+}
