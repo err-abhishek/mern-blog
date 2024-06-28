b6cc0ec1e7ac0ad8fcdd568cac482e8f9c1cb4c7
Abhishek Agrahari <abhishek737333@gmail.com>
2024-06-29 00:46:29 +0530

    Complete Sidebar of the dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import DashProfile from "../components/DashProfile";
+import DashSidebar from "../components/DashSidebar";
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (<div className="minh-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+            {/* sidebar */}
+            <DashSidebar />
+      </div>
+      {/* profile */}
+      {tab === 'profile' && <DashProfile />}
+  </div>);
 }
